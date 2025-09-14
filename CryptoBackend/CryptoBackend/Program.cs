90438e18321d01c5fd1983307fd5be7a66f826f9
lakekan <itay.cohen2907@gmail.com>
2025-09-14 15:25:21 +0300

    Allow CORS from all origins for testing

--- CryptoBackend/CryptoBackend/Program.cs
+++ CryptoBackend/CryptoBackend/Program.cs
@@ -74,25 +74,14 @@ namespace CryptoBackend
                     };
                 });
 
-                // CORS
+                // CORS - Allow all origins for now
                 builder.Services.AddCors(options =>
                 {
                     options.AddPolicy("AllowAngularApp", policy =>
                     {
-                        var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() 
-                            ?? new[] { "http://localhost:4200", "https://localhost:4200" };
-                        
-                        // Also check environment variable for CORS origins
-                        var envOrigins = builder.Configuration["Cors__AllowedOrigins"];
-                        if (!string.IsNullOrEmpty(envOrigins))
-                        {
-                            allowedOrigins = envOrigins.Split(',', StringSplitOptions.RemoveEmptyEntries);
-                        }
-                        
-                        policy.WithOrigins(allowedOrigins)
+                        policy.AllowAnyOrigin()
                               .AllowAnyHeader()
-                              .AllowAnyMethod()
-                              .AllowCredentials();
+                              .AllowAnyMethod();
                     });
                 });
 
